53e41991098546727d2bf3674b19890a2dd48cf3
hl <helin@camera360.com>
2016-08-18 10:36:28 +0800

    [何林] 增加UIKit后台调用行为检测

--- Classes/Categories/UIView+BadBackgroundBehavior.m
+++ Classes/Categories/UIView+BadBackgroundBehavior.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIView+BadBackgroundBehavior.h"
+#import <objc/runtime.h>
 
 #ifdef DEBUG
 
@@ -22,7 +23,7 @@
 {
     if (![NSThread isMainThread])
     {
-        PGAssert(NO, @"后台有地方调用了UIKit框架的接口，这种行为可能导致应用整体动画失效及其它不可预测的问题！！！如发现此断言，请在UIView+BadBackgroundBehavior类的-sc_setAnimationsEnabled:方法处打断点，查看线程调用情况！");
+        NSAssert(NO, @"后台有地方调用了UIKit框架的接口，这种行为可能导致应用整体动画失效及其它不可预测的问题！！！如发现此断言，请在UIView+BadBackgroundBehavior类的-sc_setAnimationsEnabled:方法处打断点，查看线程调用情况！");
     }
     [self sc_setAnimationsEnabled:enabled];
 }
