7b2d0adc562d5491cd9f0af33eed8c2dc9c36548
hl <helin@camera360.com>
2016-08-18 10:30:23 +0800

    [何林] 增加UIKit后台调用行为检测

---
+++ Classes/Categories/UIView+BadBackgroundBehavior.m
@@ -0,0 +1,32 @@
+//
+//  UIView+BadBackgroundBehavior.m
+//  SelfieCamera
+//
+//  Created by HeLin on 16/8/15.
+//  Copyright © 2016年 Pinguo. All rights reserved.
+//
+
+#import "UIView+BadBackgroundBehavior.h"
+
+#ifdef DEBUG
+
+@implementation UIView (BadBackgroundBehavior)
+
++ (void)load
+{
+    method_exchangeImplementations(class_getInstanceMethod(object_getClass(self), @selector(setAnimationsEnabled:)),
+                                   class_getInstanceMethod(object_getClass(self), @selector(sc_setAnimationsEnabled:)));
+}
+
++ (void)sc_setAnimationsEnabled:(BOOL)enabled
+{
+    if (![NSThread isMainThread])
+    {
+        PGAssert(NO, @"后台有地方调用了UIKit框架的接口，这种行为可能导致应用整体动画失效及其它不可预测的问题！！！如发现此断言，请在UIView+BadBackgroundBehavior类的-sc_setAnimationsEnabled:方法处打断点，查看线程调用情况！");
+    }
+    [self sc_setAnimationsEnabled:enabled];
+}
+
+@end
+
+#endif
