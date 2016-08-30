039d4f6f5dba7a59319bb7a1510f2f624cd5d38c
tl <tanglei@camera360.com>
2016-08-30 11:10:25 +0800

    [tl] 字符串是否包含emoji

--- Classes/Categories/NSString+ext.m
+++ Classes/Categories/NSString+ext.m
@@ -119,4 +119,34 @@
     }
 }
 
+//判断是否有emoji
+- (BOOL)stringContainsEmoji
+{
+    __block BOOL returnValue = NO;
+
+    [self enumerateSubstringsInRange:NSMakeRange(0, [self length])
+                               options:NSStringEnumerationByComposedCharacterSequences
+                            usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+                                const unichar high = [substring characterAtIndex: 0];
+
+                                // Surrogate pair (U+1D000-1F9FF)
+                                if (0xD800 <= high && high <= 0xDBFF) {
+                                    const unichar low = [substring characterAtIndex: 1];
+                                    const int codepoint = ((high - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+
+                                    if (0x1D000 <= codepoint && codepoint <= 0x1F9FF){
+                                        returnValue = YES;
+                                    }
+
+                                    // Not surrogate pair (U+2100-27BF)
+                                } else {
+                                    if (0x2100 <= high && high <= 0x27BF){
+                                        returnValue = YES;
+                                    }
+                                }
+                            }];
+    
+    return returnValue;
+}
+
 @end
