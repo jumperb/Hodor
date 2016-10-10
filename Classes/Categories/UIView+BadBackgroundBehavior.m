af1528ed7fa7d50bddab0ef34d89881c0bf09d3b
hl <helin@camera360.com>
2016-10-10 09:58:24 +0800

    [何林] 增加更多的UIKit后台调用检测方法

--- Classes/Categories/UIView+BadBackgroundBehavior.m
+++ Classes/Categories/UIView+BadBackgroundBehavior.m
@@ -16,17 +16,56 @@
 + (void)load
 {
     method_exchangeImplementations(class_getInstanceMethod(object_getClass(self), @selector(setAnimationsEnabled:)),
-                                   class_getInstanceMethod(object_getClass(self), @selector(sc_setAnimationsEnabled:)));
+                                   class_getInstanceMethod(object_getClass(self), @selector(h_setAnimationsEnabled:)));
+    
+    method_exchangeImplementations(class_getInstanceMethod(object_getClass(self), @selector(setNeedsLayout)),
+                                   class_getInstanceMethod(object_getClass(self), @selector(h_setNeedsLayout)));
+    
+    method_exchangeImplementations(class_getInstanceMethod(object_getClass(self), @selector(setNeedsDisplay)),
+                                   class_getInstanceMethod(object_getClass(self), @selector(h_setNeedsDisplay)));
+    
+    method_exchangeImplementations(class_getInstanceMethod(object_getClass(self), @selector(setNeedsDisplayInRect:)),
+                                   class_getInstanceMethod(object_getClass(self), @selector(h_setNeedsDisplayInRect:)));
 }
 
-+ (void)sc_setAnimationsEnabled:(BOOL)enabled
++ (void)h_setAnimationsEnabled:(BOOL)enabled
 {
     if (![NSThread isMainThread])
     {
         NSAssert(NO,
                  @"UIKit interface called on secondary thread, and that behavior will lead to all animation not work in your app!!! Please put a Breakpoint here and check all threads call stack!!!");
     }
-    [self sc_setAnimationsEnabled:enabled];
+    [self h_setAnimationsEnabled:enabled];
+}
+
+- (void)h_setNeedsDisplay
+{
+    if (![NSThread isMainThread])
+    {
+        NSAssert(NO,
+                 @"-[UIView setNeedsDisplay] called on secondary thread,  Please put a Breakpoint here and check all threads' call stack!!!");
+    }
+    [self h_setNeedsDisplay];
+}
+
+- (void)h_setNeedsLayout
+{
+    if (![NSThread isMainThread])
+    {
+        NSAssert(NO,
+                 @"-[UIView setNeedsLayout] called on secondary thread, Please put a Breakpoint here and check all threads' call stack!!!");
+    }
+    [self h_setNeedsLayout];
+}
+
+- (void)h_setNeedsDisplayInRect:(CGRect)rect
+{
+    if (![NSThread isMainThread])
+    {
+        NSAssert(NO,
+                 @"-[UIView setNeedsDisplayInRect:] called on secondary thread, Please put a Breakpoint here and check all threads' call stack!!!");
+    }
+    [self h_setNeedsDisplayInRect:rect];
 }
 
 @end
