09d04b8321d173a7af81e356c099257e44637b7c
zct <zhangchutian@camera360.com>
2017-01-11 10:56:21 +0800

    add header

--- Classes/HCommon.h
+++ Classes/HCommon.h
@@ -10,25 +10,30 @@
 #define HFramework_HCommon_h
 
 #import "HCommonBlock.h"
+#import "HDefines.h"
+#import "HFloatEqualityDefines.h"
+
+#import "HClassManager.h"
+#import "HGCDext.h"
+
 #import "NSObject+ext.h"
 #import "NSError+ext.h"
-#import "UIColor+ext.h"
 #import "NSData+ext.h"
-#import "UIImage+ext.h"
-#import "UIScreen+ext.h"
 #import "NSArray+ext.h"
 #import "NSString+ext.h"
 #import "NSDictionary+ext.h"
-#import "UIView+ext.h"
 #import "NSData+ext.h"
 #import "NSDate+ext.h"
 #import "NSFileManager+ext.h"
-#import "HDefines.h"
-#import "HFloatEqualityDefines.h"
-#import "HClassManager.h"
+
+
+#import "HGCDext.h"
+#import "UIView+ext.h"
+#import "UIImage+ext.h"
+#import "UIScreen+ext.h"
+#import "UIColor+ext.h"
 #import "UILabel+ext.h"
 #import "UIButton+ext.h"
 #import "UIApplication+ext.h"
 #import "UINavigationController+ext.h"
-#import "HGCDext.h"
 #endif
