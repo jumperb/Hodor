1186968b1280bcd9dfdf8f348fe250327dc816fb
zct <zhangchutian_05@163.com>
2017-06-15 14:09:53 +0800

    添加rgba颜色字符串支持

--- Classes/UI-Category/UIColor+ext.m
+++ Classes/UI-Category/UIColor+ext.m
@@ -22,7 +22,39 @@
 
 + (UIColor *)colorWithString:(NSString *)colorStr
 {
-    return [self colorWithString:colorStr alpha:1.0];
+    NSString *cString = [[colorStr stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    if ([cString length] < 6) return [UIColor clearColor];
+    if ([cString hasPrefix:@"0X"] || [cString hasPrefix:@"0x"]) cString = [cString substringFromIndex:2];
+    if ([cString hasPrefix:@"#"]) cString = [cString substringFromIndex:1];
+    
+    if ([cString length] != 6 && [cString length] != 8) return [UIColor clearColor];
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    NSString *aString = @"FF";
+    if (cString.length == 8)
+    {
+        range.location = 6;
+        aString = [cString substringWithRange:range];
+    }
+    unsigned int r, g, b, a;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    [[NSScanner scannerWithString:aString] scanHexInt:&a];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:((float) a / 255.0f)];
 }
 
 + (UIColor *)colorWithString:(NSString *)colorStr alpha:(float)alpha
