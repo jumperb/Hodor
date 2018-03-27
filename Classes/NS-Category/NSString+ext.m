2b8145608621c61b386ac632e71f29b31e7fdc5d
zct <zhangchutian_05@163.com>
2018-03-27 16:44:30 +0800

    fix encode bug

--- Classes/NS-Category/NSString+ext.m
+++ Classes/NS-Category/NSString+ext.m
@@ -70,15 +70,38 @@
 
 - (NSString *)encode
 {
-    NSString *outputStr =
-    (NSString *) CFBridgingRelease(
-                                   CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
-                                                                           (__bridge CFStringRef)self,
-                                                                           NULL,
-                                                                           (CFStringRef)@"!*'();:@&=+$,/?%#[]",
-                                                                           kCFStringEncodingUTF8)
-                                   );
-    return outputStr;
+    static NSString * const kAFCharactersGeneralDelimitersToEncode = @":#[]@"; // does not include "?" or "/" due to RFC 3986 - Section 3.4
+    static NSString * const kAFCharactersSubDelimitersToEncode = @"!$&'()*+,;=";
+    
+    NSMutableCharacterSet * allowedCharacterSet = [[NSCharacterSet URLQueryAllowedCharacterSet] mutableCopy];
+    [allowedCharacterSet removeCharactersInString:[kAFCharactersGeneralDelimitersToEncode stringByAppendingString:kAFCharactersSubDelimitersToEncode]];
+    
+    // FIXME: https://github.com/AFNetworking/AFNetworking/pull/3028
+    // return [string stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
+    
+    static NSUInteger const batchSize = 50;
+    
+    NSUInteger index = 0;
+    NSMutableString *escaped = @"".mutableCopy;
+    
+    while (index < self.length) {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wgnu"
+        NSUInteger length = MIN(self.length - index, batchSize);
+#pragma GCC diagnostic pop
+        NSRange range = NSMakeRange(index, length);
+        
+        // To avoid breaking up character sequences such as 👴🏻👮🏽
+        range = [self rangeOfComposedCharacterSequencesForRange:range];
+        
+        NSString *substring = [self substringWithRange:range];
+        NSString *encoded = [substring stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
+        [escaped appendString:encoded];
+        
+        index += range.length;
+    }
+    
+    return escaped;
 }
 
 - (NSString *)decode
